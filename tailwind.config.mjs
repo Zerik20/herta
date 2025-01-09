554f85a5e354f57f5ffb6aab81bd886416f4c7ab
Zerik20 <erikaban14@gmail.com>
2025-01-09 16:50:14 -0600

    Se termino la maquetacion de la pagina, falta la funcionalidad y pulir modo mobile, tambien traduccion

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,33 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: {
+					DEFAULT: '#FFC107', // Amarillo principal
+					dark: '#FFA000', // Amarillo oscuro
+					light: '#FFECB3', // Amarillo claro
+				},
+				secondary: {
+					DEFAULT: '#D32F2F', // Rojo intenso
+					dark: '#B71C1C', // Rojo más oscuro
+					light: '#FFCDD2', // Rojo claro
+				},
+				accent: {
+					DEFAULT: '#212121', // Negro sólido
+					dark: '#000000', // Negro más profundo
+					light: '#484848', // Gris oscuro para textos secundarios
+				},
+				neutral: {
+					DEFAULT: '#E0E0E0', // Gris claro
+					dark: '#172439', // Gris medio
+					light: '#F5F5F5', // Gris más claro para fondos
+				},
+				base: {
+					DEFAULT: '#FFFFFF', // Blanco puro
+				},
+			},
+		},
 	},
 	plugins: [],
-}
+};
